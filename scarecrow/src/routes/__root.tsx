14123d1d5051fec365bb062bd2174ab8632b3bbb
Sammy Thomas <sgt@cs.utexas.edu>
2024-12-12 18:32:17 +0000

    Fix scarecrow not building

--- scarecrow/src/routes/__root.tsx
+++ scarecrow/src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -15,7 +14,6 @@ function RootComponent() {
       </div>
       <hr />
       <Outlet />
-      <TanStackRouterDevtools />
     </>
   );
 }
