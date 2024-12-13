7ca4cd7dd0e4d0fb3f0ee801d2a070026bdb0fd2
Cole Vick <cole.d.vick@gmail.com>
2024-12-13 15:58:58 -0600

    small example of to_vmt macro in main
    - to run just cargo build in the top level

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use clap::Parser;
 use log::info;
 use std::{fs::File, io::Write};
+use to_vmt::to_vmt;
 use yardbird::{logger, proof_loop, YardbirdOptions};
+
+#[to_vmt]
+fn array_copy(a: Vec<usize>, mut b: Vec<usize>, mut i: usize, n: usize, z: usize) {
+    assert!(i == 0);
+    assert!(z > 0);
+    assert!(z < n);
+    loop {
+        if i < n {
+            b[i] = a[i];
+            i += 1;
+        } else {
+            break;
+        }
+    }
+    assert!(i >= n);
+    assert!(a[z] == b[z]);
+}
 
 fn main() -> anyhow::Result<()> {
     logger::init_logger();
     let options = YardbirdOptions::parse();
-
     let result = proof_loop(&options)?;
     info!("NEEDED INSTANTIATIONS: {:#?}", result.used_instances);
     if options.print_vmt {
