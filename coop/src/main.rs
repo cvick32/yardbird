5cf10b9519fcf2b03a6d012f3f33c006956c1327
Cole Vick <cole.d.vick@gmail.com>
2024-12-21 13:28:16 -0600

    [coop] add array_split_12

--- coop/src/main.rs
+++ coop/src/main.rs
@@ -34,6 +34,29 @@ fn array_copy_buggy(a: Vec<usize>, mut b: Vec<usize>, mut i: usize, n: usize, z:
     assert!(a[z] == b[z]);
 }
 
+//#[to_vmt(prover = "yardbird", timeout = 60, print_vmt = false)]
+fn array_split_12(mut a: Vec<usize>, mut i: usize, mut y: usize, n: usize, z: usize) {
+    assert!(i == 0);
+    assert!(y == 1000);
+    loop {
+        if i < n {
+            a[i] = i + y;
+            i += 1;
+            if i < 500 {
+                y -= 1;
+            } else {
+                y += 1;
+            }
+        } else {
+            break;
+        }
+    }
+    assert!(i >= n);
+    assert!(z < n);
+    assert!(z >= 500);
+    assert!((2 * z) == a[z]);
+}
+
 fn main() {
     array_copy(vec![0, 0, 0], vec![1, 1, 1], 0, 3, 2);
     array_copy_buggy(vec![0, 0, 0], vec![1, 1, 1], 0, 3, 2);
