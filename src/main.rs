18162c537f3e16f5169ce2b6716744ec1f2c4e96
Samuel Thomas <sgpthomas@gmail.com>
2025-01-03 15:53:53 -0600

    rewrite driver so that looping logic is separated from proof logic

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,45 @@
+use std::{fs::File, io::Write};
+
 use clap::Parser;
 use log::info;
-use std::{fs::File, io::Write};
-use yardbird::{concrete_z3_bmc, logger, model_from_options, repl::Repl, Driver, YardbirdOptions};
+use yardbird::{
+    logger, model_from_options,
+    repl::Repl,
+    strategies::{Abstract, ConcreteZ3, Interpolating, ProofStrategy},
+    Driver, DriverExtensions, YardbirdOptions,
+};
 
 fn main() -> anyhow::Result<()> {
     logger::init_logger();
     let options = YardbirdOptions::parse();
     let vmt_model = model_from_options(&options);
 
+    let mut driver = Driver::new(&options, &z3::Config::new(), vmt_model);
+
+    // build the strategy
+    let strat: Box<dyn ProofStrategy<_>> = match options.strategy {
+        yardbird::Strategy::Abstract => Box::new(Abstract::default()),
+        yardbird::Strategy::Concrete => Box::new(ConcreteZ3::default()),
+    };
+
+    let mut extensions = DriverExtensions::default();
     if options.interactive {
-        let repl = Repl::new(&options, &z3::Config::new(), vmt_model);
-        repl.start(options.depth, 10)?;
-        return Ok(());
+        extensions.add_extension(Repl);
     }
 
-    if options.z3 {
-        let _ = concrete_z3_bmc(&options, vmt_model);
-    } else {
-        let driver = Driver::new(&options, &z3::Config::new(), vmt_model);
-        let res = driver.check_to_depth(options.depth, 10)?;
-        info!("NEEDED INSTANTIATIONS: {:#?}", res.used_instances);
-        if options.print_vmt {
+    if options.interpolate {
+        extensions.add_extension(Interpolating);
+    }
+
+    let res = driver.check_strategy(options.depth, strat, &mut extensions)?;
+
+    info!("NEEDED INSTANTIATIONS: {:#?}", res.used_instances);
+    if options.print_vmt {
+        if let Some(model) = res.model {
             let mut output = File::create("instantiated.vmt").unwrap();
-            let _ = output.write(res.model.as_vmt_string().as_bytes());
+            let _ = output.write(model.as_vmt_string().as_bytes());
         }
     }
+
     Ok(())
 }
