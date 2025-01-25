4ae98e6e37b12783bb946f19af02f8bb180aa41e
Sammy Thomas <sgpthomas@gmail.com>
2025-01-25 14:42:41 -0600

    don't sort for extractor construction, instead just find min when we call it

--- src/extractor.rs
+++ src/extractor.rs
@@ -9,7 +9,7 @@ pub struct TermExtractor<CF>
 where
     CF: egg::CostFunction<ArrayLanguage>,
 {
-    term_map: HashMap<egg::Id, Vec<egg::RecExpr<ArrayLanguage>>>,
+    term_map: HashMap<egg::Id, Vec<(egg::RecExpr<ArrayLanguage>, CF::Cost)>>,
     cost_function: CF,
     pub reads_and_writes: ReadsAndWrites,
 }
@@ -29,23 +29,24 @@ where
     where
         N: egg::Analysis<ArrayLanguage>,
     {
-        let mut term_map: HashMap<egg::Id, Vec<egg::RecExpr<ArrayLanguage>>> = HashMap::new();
+        let mut term_map: HashMap<egg::Id, Vec<_>> = HashMap::new();
         for string_term in transition_system_terms {
             let term: egg::RecExpr<ArrayLanguage> = string_term.parse().unwrap();
+            let cost = cost_function.cost_rec(&term);
             match egraph.lookup_expr(&term) {
                 // TODO: might want to keep track of all terms that match this node
                 Some(expr) => term_map
                     .entry(expr)
-                    .and_modify(|v: &mut _| v.push(term.clone()))
-                    .or_insert_with(|| vec![term]),
+                    .and_modify(|v: &mut _| v.push((term.clone(), cost.clone())))
+                    .or_insert_with(|| vec![(term, cost)]),
                 None => continue,
             };
         }
 
         // sort terms by cost function so that the first one is the lowest cost
-        for exprs in term_map.values_mut() {
-            exprs.sort_by_key(|expr| cost_function.cost_rec(expr));
-        }
+        // for exprs in term_map.values_mut() {
+        //     exprs.sort_by_key(|(_expr, cost)| cost.clone());
+        // }
 
         Self {
             term_map,
@@ -62,13 +63,13 @@ where
     where
         N: egg::Analysis<ArrayLanguage>,
     {
-        // let is_potential_read = self.egraph[eclass]
-        //     .parents()
-        //     .any(|(node, _parent_id)| matches!(node, ArrayLanguage::Read(_)));
-
         if let Some(terms) = self.term_map.get(&egraph.find(eclass)) {
-            log::debug!("term exists: {eclass} -> {}", terms[0]);
-            terms[0].clone()
+            let (best_term, _) = terms
+                .iter()
+                .min_by_key(|(_term, cost)| cost.clone())
+                .unwrap();
+            log::debug!("term exists: {eclass} -> {}", best_term);
+            best_term.clone()
         } else {
             let extractor = egg::Extractor::new(egraph, self.cost_function.clone());
             let node = extractor.find_best_node(eclass);
