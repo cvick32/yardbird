8265d5a0e19a58d230895a6391f4e102fbc848d1
Cole Vick <cole.d.vick@gmail.com>
2026-08-31 11:11:40 -0500

    [yardbird] implement display for property check mode

--- src/solver/api.rs
+++ src/solver/api.rs
@@ -31,6 +31,16 @@ pub enum PropertyCheckMode {
     RefinementAssumptions,
 }
 
+impl std::fmt::Display for PropertyCheckMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Scoped => write!(f, "scoped"),
+            Self::Assumptions => write!(f, "assumptions"),
+            Self::RefinementAssumptions => write!(f, "refinement-assumptions"),
+        }
+    }
+}
+
 #[derive(Copy, Clone, Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum SolverCheckResult {
